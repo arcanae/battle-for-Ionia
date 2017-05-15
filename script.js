06e1b72edf72952f5d1f231e59f89f73aaf43f48
Justin <Justin>
2017-05-15 23:25:12 +0200

    titles

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ xayahLoad.addEventListener("click", function() {
     location.href = "xayah/index.html";
 });
 
+xayahLoad.addEventListener("mouseover", function() {
+    let rebel = document.querySelector("#rebel");
+    rebel.style.display = "inherit";
+});
+xayahLoad.addEventListener("mouseout", function() {
+    let rebel = document.querySelector("#rebel");
+    rebel.style.display = "none";
+});
+
 let rakanLoad = document.querySelector("#rakan-load");
 rakanLoad.addEventListener("click", function() {
     location.href = "rakan/index.html";
+});
+
+rakanLoad.addEventListener("mouseover", function() {
+    let charmer = document.querySelector("#charmer");
+    charmer.style.display = "inherit";
+});
+rakanLoad.addEventListener("mouseout", function() {
+    let charmer = document.querySelector("#charmer");
+    charmer.style.display = "none";
 });
